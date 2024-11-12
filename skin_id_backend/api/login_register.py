645d04603576d00aaa85beafbb95cb1f9b5ed8d5
Alerakha <capupucino@gmail.com>
2024-11-12 16:56:22 +0700

    backend login

--- skin_id_backend/api/login_register.py
+++ skin_id_backend/api/login_register.py
@@ -1,19 +1,17 @@
+from django.contrib.auth import authenticate
 from api.models import Pengguna
-from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
 import requests
-from django.contrib.auth.hashers import make_password
-from .services.makeup_api import fetch_products_by_category
-from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.exceptions import ValidationError
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import check_password
+import jwt
+from django.conf import settings
 
 @api_view(['POST'])
 def register_user(request):
-    """
-    Endpoint untuk registrasi pengguna baru
-    """
     if request.method == 'POST':
         # Ambil data dari request
         username = request.data.get('username')
@@ -40,4 +38,41 @@ def register_user(request):
             'email': pengguna.email,
             'message': 'User berhasil dibuat'
         }, status=status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+def login_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if not username or not password:
+        raise ValidationError("Username dan password diperlukan")
+    
+    try:
+        pengguna = Pengguna.objects.filter(username=username).first()
+        # cek pengguna apakah sudah ada di database
+        if pengguna is None:
+            return Response({'Error':'Username dengan nama tersebut tidak ditemukan'}, status=status.HTTP_404_NOT_FOUND)
+        # verifikasi password
+        if not check_password(password, pengguna.password):
+            return Response({'Error':'Password salah'}, status=status.HTTP_400_BAD_REQUEST)
         
+        payload = {
+            'user_id': pengguna.user_id,
+            'username': pengguna.username,
+            'email': pengguna.email,
+        }
+        
+        token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+        
+        return Response({
+            'message': 'Login berhasil',
+            'token': token,
+            'user': {
+                'id': pengguna.user_id,
+                'username': pengguna.username,
+                'email': pengguna.email
+            }
+        }, status=status.HTTP_200_OK)
+    
+    except Exception as e:
+        return Response({'error':str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- skin_id_backend/api/urls.py
+++ skin_id_backend/api/urls.py
@@ -4,9 +4,11 @@ from . import views
 from .ml_model import predict_skin_tone_view
 from .makeup_product import fetch_filtered_makeup_products
 from .login_register import register_user
+from .login_register import login_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',login_user, name='login-user'),
     path('register/',register_user, name='register-user'),
     path('makeup-products/', fetch_filtered_makeup_products, name='makeup-products'),
     path('predict/', predict_skin_tone_view, name='predict-skin-tone')
