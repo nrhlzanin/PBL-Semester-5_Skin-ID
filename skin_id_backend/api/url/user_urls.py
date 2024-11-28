1184eba77ce7d108446d90ecfc77b7be34fcd40e
Alerakha <capupucino@gmail.com>
2024-11-28 14:25:59 +0700

    recommendation up to database

--- skin_id_backend/api/url/user_urls.py
+++ skin_id_backend/api/url/user_urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from api.views.user_views import register_user, login_user, edit_profile, user_logout, get_user_profile, verify_email
 from api.machine_learning.ml_model import predict_skin_tone_view
-from api.views.makeup_product import fetch_filtered_makeup_products, fetch_makeup_products, recommend_product
+from api.views.makeup_product import fetch_filtered_makeup_products, fetch_makeup_products, recommend_product, get_recommendations
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
@@ -12,5 +12,6 @@ urlpatterns = [
     path('makeup-products/', fetch_filtered_makeup_products, name='makeup-products'),
     path('all-makeup-products/', fetch_makeup_products, name='all-makeup-products'),
     path('predict/', predict_skin_tone_view, name='predict-skin-tone'),
-    path('recommendations/', recommend_product, name='foundation-recommendation'),
+    path('recommendations/', recommend_product, name='make-recommendation'),
+    path('get-recommendations/', get_recommendations, name='get-recommendation'),
 ]
