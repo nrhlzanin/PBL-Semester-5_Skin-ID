b4b6d6d1e1056b95cee8e48a8530178afffdf270
YunilaPutmasari <nilaputma26@gmail.com>
2024-11-30 01:18:05 +0700

    perbaikan shared_preferences di login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,31 +1,20 @@
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:skin_id/screen/home_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Login extends StatefulWidget {
+  const Login({super.key});
+
   @override
   _LoginAccountState createState() => _LoginAccountState();
 }
 
 class _LoginAccountState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-
-  bool _isLoading = false;
-  String? _errorMessage;
-
-  String? _validateUsername(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Please enter your username';
-    } else if (value.length < 3) {
-      return 'Username must be at least 3 characters';
-    }
-    return null;
-  }
 
   String? _validateEmail(String? value) {
     if (value == null || value.isEmpty) {
@@ -47,161 +36,219 @@ class _LoginAccountState extends State<Login> {
   }
 
   Future<void> loginUser(String email, String password) async {
-    setState(() {
-      _isLoading = true;
-      _errorMessage = null;
-    });
+    final response = await http.post(
+      Uri.parse('http://192.168.1.4:8000/api/user/login/'),
+      body: {'email': email, 'password': password},
+    );
 
-    final url = Uri.parse('http://192.168.1.4:8000/api/login/'); // Sesuaikan URL API
-    final headers = {'Content-Type': 'application/json'};
-    final body = jsonEncode({
-      'email': email,
-      'password': password, // Kirim email dan password ke server
-    });
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final token = data['token'];
 
-    try {
-      final response = await http.post(url, headers: headers, body: body);
+      // Save token to SharedPreferences
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('auth_token', token);
 
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
+      print('Login successful. Token saved.');
 
-        // Simpan token dan data pengguna di SharedPreferences
-        final prefs = await SharedPreferences.getInstance();
-        await prefs.setString('token', data['token']);
-        await prefs.setString('username', data['username']);
-        await prefs.setString('email', data['email']);
+      // Navigate to HomeScreen after successful login
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    } else {
+      print('Login failed: ${response.body}');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Login failed: ${response.body}')),
+      );
+    }
+  }
 
-        // Navigasi ke halaman Home
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => HomePage()),
-        );
-      } else if (response.statusCode == 401) {
-        setState(() {
-          _errorMessage = 'Invalid email or password';
-        });
-      } else {
-        setState(() {
-          _errorMessage = 'Unexpected error occurred';
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _errorMessage = 'Failed to connect to server';
-      });
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
+  // Check if token already exists in SharedPreferences when app starts
+  Future<void> checkToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('auth_token');
+
+    if (token != null) {
+      // If token exists, navigate directly to HomeScreen
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
     }
   }
 
   @override
+  void initState() {
+    super.initState();
+    checkToken(); // Check token when the app starts
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          // Background and overlay
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/image/makeup.jpg"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color.fromARGB(158, 163, 85, 56),
-                  Color(0xFFB68D40).withOpacity(0.5),
-                  Color.fromARGB(255, 39, 39, 39).withOpacity(0.5),
-                ],
-              ),
-            ),
-          ),
-          Center(
-            child: Container(
-              padding: EdgeInsets.all(20),
-              margin: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Hides the debug banner
+      home: Scaffold(
+        extendBody: true,
+        extendBodyBehindAppBar: true,
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            // Background image
+            Container(
               decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.8),
-                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                  image: AssetImage("assets/image/makeup.jpg"),
+                  fit: BoxFit.cover,
+                ),
               ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('Welcome Back!', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      controller: _usernameController,
-                      decoration: InputDecoration(labelText: 'Username', hintText: 'Type your username', border: OutlineInputBorder()),
-                      validator: _validateUsername,
-                    ),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      controller: _emailController,
-                      decoration: InputDecoration(labelText: 'Email', hintText: 'Type your email', border: OutlineInputBorder()),
-                      validator: _validateEmail,
-                    ),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(labelText: 'Password', hintText: 'Enter your password', border: OutlineInputBorder()),
-                      validator: _validatePassword,
-                    ),
-                    SizedBox(height: 10),
-                    if (_errorMessage != null) ...[
-                      Text(_errorMessage!, style: TextStyle(color: Colors.red)),
-                      SizedBox(height: 16.0),
-                    ],
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          final email = _emailController.text.trim();
-                          final password = _passwordController.text.trim();
-                          loginUser(email, password);
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(backgroundColor: Colors.black, padding: EdgeInsets.symmetric(vertical: 15, horizontal: 50), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
-                      child: Center(child: Text('Login', style: TextStyle(color: Colors.white))),
-                    ),
-                    SizedBox(height: 10),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        print("Continue with Google");
-                      },
-                      icon: Image.asset("assets/image/Logo-google-icon-PNG.png", height: 20, width: 20),
-                      label: Text('Continue with Google', style: TextStyle(color: Colors.black)),
-                      style: ElevatedButton.styleFrom(backgroundColor: Colors.white, side: BorderSide(color: Colors.grey), padding: EdgeInsets.symmetric(vertical: 13, horizontal: 73), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
-                    ),
-                    SizedBox(height: 10),
-                    GestureDetector(
-                      onTap: () {
-                        print("Navigating to Create Account");
-                      },
-                      child: RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
-                          children: [
-                            TextSpan(text: 'Create an Account', style: TextStyle(color: Colors.black, fontSize: 10, fontWeight: FontWeight.bold, fontFamily: 'Montserrat', decoration: TextDecoration.underline)),
-                            TextSpan(text: ' if you don’t have one yet', style: TextStyle(color: Colors.black, fontSize: 10, fontFamily: 'Montserrat', fontWeight: FontWeight.w300)),
-                          ],
-                        ),
-                      ),
-                    ),
+            ),
+            // Gradient overlay
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(158, 163, 85, 56),
+                    Color(0xFFB68D40).withOpacity(0.5),
+                    Color.fromARGB(255, 39, 39, 39).withOpacity(0.5),
                   ],
                 ),
               ),
             ),
-          ),
-        ],
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Welcome Back!',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      // Email TextFormField with validation
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          hintText: 'Type your email',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: _validateEmail,
+                      ),
+                      SizedBox(height: 20),
+                      // Password TextFormField with validation
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          hintText: 'Enter your password',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: _validatePassword,
+                      ),
+                      SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            final email = _emailController.text.trim();
+                            final password = _passwordController.text.trim();
+                            loginUser(email, password);
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          padding: EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          print("Continue with Google");
+                        },
+                        icon: Image.asset(
+                          "assets/image/Logo-google-icon-PNG.png",
+                          height: 20,
+                          width: 20,
+                        ),
+                        label: Text(
+                          'Continue with Google',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          side: BorderSide(color: Colors.grey),
+                          padding: EdgeInsets.symmetric(
+                            vertical: 13,
+                            horizontal: 73,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      GestureDetector(
+                        onTap: () {
+                          print("Navigating to Create Account");
+                        },
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'Create an Account',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat',
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                              TextSpan(
+                                text: ' if you don’t have one yet',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 10,
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
